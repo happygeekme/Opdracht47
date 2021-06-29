0a4b747a27008257583d5a204ffdefc7b9d732bd
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2021-06-29 10:21:30 +0200

    opdracht 47

---
+++ add-one.js
@@ -0,0 +1,11 @@
+const addOne = function(numbers) {
+    // Schrijf hier de functie...
+    const result =  numbers.map(number => number + 1);
+    console.log(result);
+    return result;
+}
+
+module.exports = addOne;
+
+
+
---
+++ get-word-lengths.js
@@ -0,0 +1,5 @@
+const getWordLengths = function(someWords) {
+    //Schrijf je functie...
+   someWords.map(words => words.length)
+}
+module.exports = getWordLengths;
